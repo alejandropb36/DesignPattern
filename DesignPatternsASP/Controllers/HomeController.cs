345fe34806450c9dee6d2d3e19b2c4f64db899d0
Alejandro Ponce <alexponce36@hotmail.com>
2021-10-14 18:16:57 -0500

    Implementando Singleton en ASP

---
+++ DesignPatternsASP/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DesignPatternsASP.Models;
+using Tools;
+using Microsoft.Extensions.Options;
+using DesignPatternsASP.Configuration;
+
+namespace DesignPatternsASP.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly IOptions<MyConfig> _config;
+
+        public HomeController(IOptions<MyConfig> config)
+        {
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            Log.GetInstance(_config.Value.PathLog).Save("Entro a index");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            Log.GetInstance(_config.Value.PathLog).Save("Entro a privacy");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
