6c5206846aa97f1cf251a1c5cc85b4194ca6f677
Alejandro Ponce <alexponce36@hotmail.com>
2021-11-02 17:42:58 -0600

    Factory Method Explicacion

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DesignPattern.FactoryPattern;
+using System;
 
 namespace DesignPattern
 {
@@ -6,9 +7,14 @@ namespace DesignPattern
     {
         static void Main(string[] args)
         {
-            var log = Singleton.Log.Instance;
-            log.Save("a");
-            log.Save("b");
+            SaleFactory storeSaleFactory = new StoreSaleFactory(10);
+            SaleFactory internetSaleFactory = new InternetSaleFactory(2);
+
+            ISale sale1 = storeSaleFactory.GetSale();
+            sale1.Sell(15);
+
+            ISale sale2 = internetSaleFactory.GetSale();
+            sale2.Sell(20);
         }
     }
 }
