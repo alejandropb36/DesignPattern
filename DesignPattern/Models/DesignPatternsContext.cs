4c72b8d87348a02171fe20b9609cdd11c0033f2b
Alejandro Ponce <alexponce36@hotmail.com>
2021-12-06 21:18:33 -0600

    Entity Framework

---
+++ DesignPattern/Models/DesignPatternsContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace DesignPattern.Models
+{
+    public partial class DesignPatternsContext : DbContext
+    {
+        public DesignPatternsContext()
+        {
+        }
+
+        public DesignPatternsContext(DbContextOptions<DesignPatternsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Beer> Beer { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=localhost; Database=DesignPatterns; Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Beer>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Style)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,6 +1,8 @@
 ﻿using DesignPattern.DependencyInjection;
 using DesignPattern.FactoryPattern;
+using DesignPattern.Models;
 using System;
+using System.Linq;
 
 namespace DesignPattern
 {
@@ -8,18 +10,27 @@ namespace DesignPattern
     {
         static void Main(string[] args)
         {
-            SaleFactory storeSaleFactory = new StoreSaleFactory(10);
-            SaleFactory internetSaleFactory = new InternetSaleFactory(2);
+            //SaleFactory storeSaleFactory = new StoreSaleFactory(10);
+            //SaleFactory internetSaleFactory = new InternetSaleFactory(2);
 
-            ISale sale1 = storeSaleFactory.GetSale();
-            sale1.Sell(15);
+            //ISale sale1 = storeSaleFactory.GetSale();
+            //sale1.Sell(15);
 
-            ISale sale2 = internetSaleFactory.GetSale();
-            sale2.Sell(20);
+            //ISale sale2 = internetSaleFactory.GetSale();
+            //sale2.Sell(20);
 
-            var beer = new Beer("Stout", "Minerva");
-            var drinkWithBeer = new DrinkWithBeer(10, 10, beer);
-            drinkWithBeer.Build();
+            //var beer = new Beer("Stout", "Minerva");
+            //var drinkWithBeer = new DrinkWithBeer(10, 10, beer);
+            //drinkWithBeer.Build();
+
+            using(var context = new DesignPatternsContext())
+            {
+                var beers = context.Beer.ToList();
+                foreach(var beer in beers)
+                {
+                    Console.WriteLine(beer.Name);
+                }
+            }
         }
     }
 }
