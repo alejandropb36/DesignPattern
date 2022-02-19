4baae2912243c5b87bd52d5706b6957489d704a9
Alejandro Ponce <alexponce36@hotmail.com>
2022-02-19 13:04:02 -0600

    feat: change connection string and test

--- DesignPattern/Models/Brand.cs
+++ DesignPattern/Models/Brand.cs
@@ -9,7 +9,7 @@ namespace DesignPattern.Models
 {
     public partial class Brand
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
     }
 }
--- DesignPattern/Models/DesignPatternsContext.cs
+++ DesignPattern/Models/DesignPatternsContext.cs
@@ -27,7 +27,7 @@ namespace DesignPattern.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=localhost; Database=DesignPatterns;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=localhost;Database=DesignPatterns;User Id=sa;Password=secret123;");
             }
         }
 
