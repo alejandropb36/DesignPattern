7b8c820d71d8fcd7f0d1c47d5c6d9f80311e601d
Alejandro Ponce <alexponce36@hotmail.com>
2022-02-20 11:50:07 -0600

    feat: UnitOfWork Guardando en dos tablas

--- DesignPatternsASP/Controllers/BeerController.cs
+++ DesignPatternsASP/Controllers/BeerController.cs
@@ -1,6 +1,9 @@
-﻿using DesignPatterns.Repository;
+﻿using DesignPatterns.Models.Data;
+using DesignPatterns.Repository;
 using DesignPatternsASP.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -26,6 +29,48 @@ namespace DesignPatternsASP.Controllers
                                                };
 
             return View("Index", beers);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var brands = _unitOfWork.Brands.Get();
+            ViewBag.Brands = new SelectList(brands, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(FormBeerViewModel formBeerViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                var brands = _unitOfWork.Brands.Get();
+                ViewBag.Brands = new SelectList(brands, "Id", "Name");
+                return View("Add", formBeerViewModel);
+            }
+
+            var beer = new Beer();
+            beer.Name = formBeerViewModel.Name;
+            beer.Style = formBeerViewModel.Style;
+
+            if (!string.IsNullOrEmpty(formBeerViewModel.OtherBrand) && (formBeerViewModel.BrandId == null || formBeerViewModel.BrandId == Guid.Empty))
+            {
+                var brand = new Brand();
+                brand.Name = formBeerViewModel.OtherBrand;
+                brand.Id = Guid.NewGuid();
+
+                beer.BrandId = brand.Id;
+                _unitOfWork.Brands.Add(brand);
+            }
+            else
+            {
+                beer.BrandId = formBeerViewModel.BrandId;
+            }
+
+            _unitOfWork.Beers.Add(beer);
+            _unitOfWork.Save();
+
+            return RedirectToAction("Index");
         }
     }
 }
